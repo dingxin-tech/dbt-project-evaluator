0b89fb52b625f99bd61bd99990265d70677f8320
候高杰 <hgj02217875@alibaba-inc.com>
2025-03-31 18:11:37 +0800

    将CTE改为子查询，避免CTE导致的数据类型问题。 group by (n)，order by (n)改为+列名。 target.type添加maxcompute。

--- models/marts/dag/fct_model_fanout.sql
+++ models/marts/dag/fct_model_fanout.sql
@@ -1,51 +1,45 @@
-with all_dag_relationships as (
-    select  
-        *
-    from {{ ref('int_all_dag_relationships') }}
-    where not parent_is_excluded
-    and not child_is_excluded
-),
-
--- find all models without children
-models_without_children as (
-    select
-        parent
-    from all_dag_relationships
-    where parent_resource_type = 'model'
-    group by 1
-    having max(distance) = 0
-),
-
--- all parents with more direct children than the threshold for fanout (determined by variable models_fanout_threshold, default 3)
-    -- Note: only counts "leaf children" - direct chilren that are models AND are child-less (are at the right-most-point in the DAG)
-model_fanout as (
-    select 
-        all_dag_relationships.parent,
-        all_dag_relationships.parent_model_type,
-        all_dag_relationships.child
-    from all_dag_relationships
-    inner join models_without_children
-        on all_dag_relationships.child = models_without_children.parent
-    where all_dag_relationships.distance = 1 and all_dag_relationships.child_resource_type = 'model'
-    group by 1, 2, 3
-    -- we order the CTE so that listagg returns values correctly sorted for some warehouses
-    order by 1, 2, 3
-),
-
-model_fanout_agg as (
+select * from (
     select
         parent,
         parent_model_type,
         {{ dbt.listagg(
-            measure = 'child', 
-            delimiter_text = "', '", 
-            order_by_clause = 'order by child' if target.type in ['snowflake','redshift','duckdb','trino'])
+            measure = 'child',
+            delimiter_text = "', '",
+            order_by_clause = 'order by child' if target.type in ['snowflake','redshift','duckdb','trino','maxcompute'])
         }} as leaf_children
-    from model_fanout
-    group by 1, 2
+    from (
+    select
+        all_dag_relationships.parent,
+        all_dag_relationships.parent_model_type,
+        all_dag_relationships.child
+    from (
+    select
+        *
+    from {{ ref('int_all_dag_relationships') }}
+    where not parent_is_excluded
+    and not child_is_excluded
+) as all_dag_relationships
+    inner join (
+    select
+        parent
+    from (
+    select
+        *
+    from {{ ref('int_all_dag_relationships') }}
+    where not parent_is_excluded
+    and not child_is_excluded
+) as all_dag_relationships
+    where parent_resource_type = 'model'
+    group by parent
+    having max(distance) = 0
+) as models_without_children
+        on all_dag_relationships.child = models_without_children.parent
+    where all_dag_relationships.distance = 1 and all_dag_relationships.child_resource_type = 'model'
+    group by all_dag_relationships.parent,all_dag_relationships.parent_model_type,all_dag_relationships.child
+    order by all_dag_relationships.parent,all_dag_relationships.parent_model_type,all_dag_relationships.child
+) as model_fanout
+    group by parent,parent_model_type
     having count(*) >= {{ var('models_fanout_threshold') }}
-)
-
-select * from model_fanout_agg
-
+) as model_fanout_agg
+where 1=1
 {{ filter_exceptions() }}

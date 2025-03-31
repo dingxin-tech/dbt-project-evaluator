daca2bcfdbefb3cea29a196ebe7db31c1cda229c
候高杰 <hgj02217875@alibaba-inc.com>
2025-03-31 18:09:43 +0800

    将CTE改为子查询，避免CTE导致的数据类型问题。 group by (n)，order by (n)改为+列名。

--- models/marts/dag/fct_root_models.sql
+++ models/marts/dag/fct_root_models.sql
@@ -1,25 +1,19 @@
--- this model finds cases where a model has 0 direct parents, likely due to a lack of source or ref function
-
-with model_relationships as (
-    select  
+select * from (
+    select
+        child
+    from (
+    select
         *
     from {{ ref('int_all_dag_relationships') }}
     where child_resource_type = 'model'
     -- only filter out excluded children nodes
-        -- filtering parents could result in incorrectly flagging nodes that depend on excluded nodes
+    -- filtering parents could result in incorrectly flagging nodes that depend on excluded nodes
     and not child_is_excluded
     -- exclude required time spine
     and child != 'metricflow_time_spine'
-),
-
-final as (
-    select
-        child
-    from model_relationships
-    group by 1
+) as model_relationships
+    group by child
     having max(distance) = 0
-)
-
-select * from final
-
+) as final
+where 1=1
 {{ filter_exceptions() }}
